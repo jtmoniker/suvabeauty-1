d598e05335c9f60a585137b2c38522397982276d
Erik Tammpere <erik@Eriks-Air.hitronhub.home>
2019-05-23 00:01:39 -0700

    [ADD] added total weight indicator on sales order form

--- suva_product_fields/__manifest__.py
+++ suva_product_fields/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "suva_product_fields",
+    'name': "Suva Custom Fields",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -20,16 +20,10 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['product', 'website_sale'],
+    'depends': ['delivery'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
