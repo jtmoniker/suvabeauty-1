4070e12b6a023ad67321ec8bdbff7c721cdd019d
Erik Tammpere <erik@Eriks-Air.hitronhub.home>
2019-05-23 21:15:40 -0700

    [FIX] Dependency error.

--- account_taxcloud_remote_origin/__manifest__.py
+++ account_taxcloud_remote_origin/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account_taxcloud', 'sale'],
+    'depends': ['sale', 'account_taxcloud', 'sale_account_taxcloud'],
 
     # always loaded
     'data': [
