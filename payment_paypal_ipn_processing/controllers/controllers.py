26e8b2c8faeaaba365eaf542fd5aee1b567d6e5f
Erik Tammpere <erik@Eriks-MacBook-Air.local>
2019-07-03 17:18:23 -0700

    [ADD] New module to process orders after paypal transactions validated via ipn notification

---
+++ payment_paypal_ipn_processing/controllers/controllers.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import logging
+import pprint
+
+from odoo import http
+from odoo.http import request
+from odoo.addons.payment_paypal.controllers.main import PaypalController
+
+_logger = logging.getLogger(__name__)
+
+class PaypalController(PaypalController):
+
+	@http.route('/payment/paypal/ipn/')
+	def paypal_ipn(self, **post):
+		""" Paypal IPN. """
+		_logger.info('Beginning Paypal IPN form_feedback with post data %s', pprint.pformat(post))  # debug
+		try:
+			self.paypal_validate_data(**post)
+		except ValidationError:
+			_logger.exception('Unable to validate the Paypal payment')
+		else:
+			tx = request.env['payment.transaction'].search([('reference', '=', post.get('item_number'))])
+			try:
+				tx._post_process_after_done()
+				self.env.cr.commit()
+			except Exception as e:
+				_logger.exception("Transaction post processing failed")
+				self.env.cr.rollback()
+		return ''
